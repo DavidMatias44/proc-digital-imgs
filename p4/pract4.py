1291e9807942f4e44cf9313017ce7b00c700a3ab
DavidMatias44 <daosmaru3667@gmail.com>
2024-11-11 22:50:12 -0600

    Avances en el menú de opciones, implementación del histograma local e implementación de funciones para mostrar resultados.

--- p4/pract4.py
+++ p4/pract4.py
@@ -9,10 +9,11 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-def read_images(image_paths: list[str]) -> list[np.array]:
+
+def read_images(image_paths: dict) -> list[np.array]:
     images = []
 
-    for image_path in image_paths:
+    for image_path in image_paths.values():
         image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
         if image is None: 
             print(f"Image not found on path: {image_path}")
@@ -23,19 +24,52 @@ def read_images(image_paths: list[str]) -> list[np.array]:
     return images
 
 
+def read_image(image_path: str) -> np.array:
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    if image is None:
+        print(f"Image not found on path: {image_path}")
+        exit(1)
+    
+    return image
+
+# TODO: esto
+def write_images(images: list[np.array], result_image_paths: list[str]) -> None:
+    pass
+
+# TODO: dar nombre a las imagenes; la original y el resultado (metodo usado).
+def show_images(original, result) -> None:
+    plt.subplot(1, 2, 1)
+    # plt.title("Imagen original (alto contraste).")
+    plt.imshow(original, cmap='gray', vmin=0, vmax=255)
+    plt.axis('off')
+
+    plt.subplot(1, 2, 2)
+    # plt.title("Imagen ecualización de histograma global (alto contraste).")
+    plt.imshow(result, cmap='gray', vmin=0, vmax=255)
+    plt.axis('off')
+
+    plt.show()
+
+
 def equalize_hist(image: np.array) -> np.array:
     return cv2.equalizeHist(image)
 
 
+def equalize_hist_local(image: np.array) -> np.array:
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    return clahe.apply(image)
+
+
 if __name__ == "__main__":
-    image_paths = [
-        "imgs/alto_contraste.jpg",
-        "imgs/bajo_contraste.jpg",
-        "imgs/alta_iluminacion.jpg",
-        "imgs/baja_iluminacion.jpg"
-    ]
+    image_paths = {
+        '1': "imgs/alto_contraste.jpg",
+        '2': "imgs/bajo_contraste.jpg",
+        '3': "imgs/alta_iluminacion.jpg",
+        '4': "imgs/baja_iluminacion.jpg"
+    }
     img_ac, img_bc, img_ai, img_bi = read_images(image_paths)
 
+    # TODO: dependiendo si el directorio existe, abrir, procesar y guardar TODAS las imagenes.
     # las imagenes resultado se guardan en un subdirectorio en esta misma carpeta. 
     current_path = os.path.dirname(os.path.abspath(__file__))
     img_result_directory = "img-resultados" 
@@ -55,17 +89,30 @@ if __name__ == "__main__":
     img_bi_global_hist = equalize_hist(img_bi)
     cv2.imwrite(img_result_directory + '/bi_histograma_global.jpeg', img_bi_global_hist)
 
-    # TODO: mejorar la forma en la que se muestran las imágenes, tal vez en dos ventanas separadas para que se comparen mejor.
-    plt.figure(figsize=(15, 10))
 
-    plt.subplot(1, 2, 1)
-    plt.title("Imagen original (alto contraste).")
-    plt.imshow(img_ac, cmap='gray')
-    plt.axis('off')
+    while True:
+        print("\nOpciones (imágenes): ")
+        print("  [1]: Alto contraste.")
+        print("  [2]: Bajo contraste.")
+        print("  [3]: Alta iluminación.")
+        print("  [4]: Baja iluminación")
+        print("  [5]: Salir.")
 
-    plt.subplot(1, 2, 2)
-    plt.title("Imagen ecualización de histograma global (alto contraste).")
-    plt.imshow(img_ac_global_hist, cmap='gray')
-    plt.axis('off')
+        image_option = input("Elija un numero (1-5): ")
+        if image_option in image_paths: image = read_image(image_paths[image_option])
+        elif image_option == "5": break
 
-    plt.show()
+        print("\nOpciones (ecualización de histograma): ")
+        print("  [1]: Local.")
+        print("  [2]: Global.")
+        print("  [3]: Cancelar.")
+
+        equalization_option = input("Elija un numero (1-3): ")
+        if equalization_option == '1':
+            result = equalize_hist(image)
+            show_images(image, result)
+        elif equalization_option == '2': 
+            result = equalize_hist_local(image)
+            show_images(image, result)
+        elif equalization_option == '3': continue
+        
