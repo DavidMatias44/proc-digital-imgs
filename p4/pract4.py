2fe6ab2f81d883755fdec103323fdc1ac3c46e47
DavidMatias44 <daosmaru3667@gmail.com>
2024-11-13 00:09:48 -0600

    Correcciones en la implementacion del histograma global.

--- p4/pract4.py
+++ p4/pract4.py
@@ -9,17 +9,18 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
+MAX_INTENSITY = 256
+
 
 def read_images(image_paths: dict) -> list[np.array]:
     images = []
-
     for image_path in image_paths.values():
         image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
         if image is None: 
             print(f"Image not found on path: {image_path}")
             exit(1)
 
-        images.append(image)
+        images.append(np.int8(image))
     
     return images
 
@@ -31,6 +32,7 @@ def read_image(image_path: str) -> np.array:
         exit(1)
     
     return image
+
 
 # TODO: esto
 def write_images(images: list[np.array], result_image_paths: list[str]) -> None:
@@ -51,13 +53,45 @@ def show_images(original, result) -> None:
     plt.show()
 
 
-def equalize_hist(image: np.array) -> np.array:
-    return cv2.equalizeHist(image)
-
-
 def equalize_hist_local(image: np.array) -> np.array:
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
     return clahe.apply(image)
+
+
+def local_eq_hist(image: np.array) -> np.array:
+    pass
+    
+
+def global_eq_hist(image: np.array) -> np.array:
+    histogram = get_histogram(image)
+    pdf = get_PDF(histogram)
+
+    result = pdf[image]
+    return result 
+
+
+def get_histogram(image: np.array) -> np.array:
+    result = np.zeros(MAX_INTENSITY)    
+    image_size = image.shape[0] * image.shape[1]
+
+    for row in image:
+        for pixel in row:
+            result[pixel] += 1
+
+    return result / image_size
+
+
+def get_PDF(histogram: np.array) -> np.array:
+    result = np.zeros(MAX_INTENSITY)
+    histogram = histogram * (MAX_INTENSITY - 1)
+
+    for index in range(MAX_INTENSITY):
+        if index == 0:
+            result[index] = histogram[index]
+        else:
+            result[index] = result[index - 1] + histogram[index]
+
+    return np.uint8(np.round(result))
 
 
 if __name__ == "__main__":
@@ -67,6 +101,7 @@ if __name__ == "__main__":
         '3': "imgs/alta_iluminacion.jpg",
         '4': "imgs/baja_iluminacion.jpg"
     }
+    aux = {"1": "ac", "2": "bc", "3": "ai", "4": "bi"}
     img_ac, img_bc, img_ai, img_bi = read_images(image_paths)
 
     # TODO: dependiendo si el directorio existe, abrir, procesar y guardar TODAS las imagenes.
@@ -76,20 +111,6 @@ if __name__ == "__main__":
     img_result_path = os.path.join(current_path, img_result_directory)
     os.makedirs(img_result_path, exist_ok=True)
     
-    # TODO: mejorar esta parte, hacerlo en una funcion o yo qué sé.
-    img_ac_global_hist = equalize_hist(img_ac)
-    cv2.imwrite(img_result_directory + '/ac_histograma_global.jpeg', img_ac_global_hist)
-
-    img_bc_global_hist = equalize_hist(img_bc)
-    cv2.imwrite(img_result_directory + '/bc_histograma_global.jpeg', img_bc_global_hist)
-
-    img_ai_global_hist = equalize_hist(img_ai)
-    cv2.imwrite(img_result_directory + '/ai_histograma_global.jpeg', img_ai_global_hist)
-
-    img_bi_global_hist = equalize_hist(img_bi)
-    cv2.imwrite(img_result_directory + '/bi_histograma_global.jpeg', img_bi_global_hist)
-
-
     while True:
         print("\nOpciones (imágenes): ")
         print("  [1]: Alto contraste.")
@@ -109,10 +130,12 @@ if __name__ == "__main__":
 
         equalization_option = input("Elija un numero (1-3): ")
         if equalization_option == '1':
-            result = equalize_hist(image)
-            show_images(image, result)
-        elif equalization_option == '2': 
             result = equalize_hist_local(image)
             show_images(image, result)
+            cv2.imwrite(f"{img_result_directory}/{aux[image_option]}-local.jpeg", result)
+        elif equalization_option == '2': 
+            result = global_eq_hist(image)
+            show_images(image, result)
+            cv2.imwrite(f"{img_result_directory}/{aux[image_option]}-global.jpeg", result)
         elif equalization_option == '3': continue
         
