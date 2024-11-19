8533d506f93c8b5aa24831d57b244b3695566020
DavidMatias44 <daosmaru3667@gmail.com>
2024-11-19 07:47:00 -0600

    Inicio de la práctica.

---
+++ p5/pract5.py
@@ -0,0 +1,35 @@
+# Utilizando una herramienta de PDI, realice los siguiente.
+# 1. Descargue cuatro imágenes: alto y bajo contraste, y alta y baja iluminación.
+# 2. Implemente los filtros suavizantes y realzantes: promedio, mediana, máximo y mínimo,
+#    Laplaciano y gradiente. Deberá utilizar distintos tamaños de máscaras (3x3, 5x5, etc.),
+#    excepto para el Laplaciano y para el gradiente.
+# 3. Pruebe los filtros suavizantes y realzantes sobre las imágenes descargadas en el punto 1.
+#    Deberá recomendar un filtro para cada imagen.
+
+def filtro_promedio():
+    pass
+
+
+def filtro_mediana():
+    pass
+
+
+def filtro_maximo():
+    pass
+
+
+def filtro_minimo():
+    pass
+
+
+def filtro_laplaciano():
+    pass
+
+
+def filtro_gradiente():
+    pass
+
+
+if __name__ == "__main__":
+    
+    pass
